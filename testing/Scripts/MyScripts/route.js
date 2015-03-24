d1a78fe421df928904d6d1d0c4a46f6a53ddb9e5
yatin sharma <sharmayatin489@gmail.com>
2015-03-24 11:30:01 +0530

    sample

---
+++ testing/Scripts/MyScripts/route.js
@@ -0,0 +1,17 @@
+﻿app.config(function ($routeProvider) {
+
+    $routeProvider.when("/Admin", {
+        templateUrl: "/Home/Index",
+        controller: "crudController"
+    }
+    )
+    $routeProvider.when("/User", {
+        templateUrl: "/Home/User",
+        controller: "crudController"
+    }
+)
+        .otherwise("/User");
+
+    
+
+});
